4e618e53e96e1ccc36c7dd9cda49fabcd477c980
roufy235 <roufy235@gmail.com>
2022-08-24 16:42:58 +0100

    feat: bug fixes

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -2,7 +2,7 @@
 const String appLogo = 'assets/images/logo_dark.svg';
 const String checkMark = 'assets/svg/check.svg';
 const String successSvgImage = 'assets/svg/success.svg';
-const String appleIconLight = 'assets/svg/apple_white.png';
+const String appleIconLight = 'assets/images/apple_white.png';
 const String appleIconDark = 'assets/images/apple.png';
 const String facebookIcon = 'assets/images/facebook.png';
 const String googleIcon = 'assets/images/google.png';
